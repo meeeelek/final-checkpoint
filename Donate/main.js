a2e70006347f0ecf46aae9132c19c44b57b6fe3e
unknown <malek amri>
2022-08-30 13:04:04 +0100

    first commit

---
+++ Donate/main.js
@@ -0,0 +1,24 @@
+
+function getinfo(){
+ 
+    var fullname = document.getElementById("fullname").value
+    var nameoncard = document.getElementById("nameoncard").value
+    var address = document.getElementById("address").value
+    var city = document.getElementById("city").value
+    var email = document.getElementById("email").value
+    var cardnumber = document.getElementById("cardnumber").value
+    var cardcvc = document.getElementById("cardcvc").value
+
+
+
+   document.getElementById('fullnamee').innerText=fullname
+   document.getElementById('nameoncardd').innerText=nameoncard
+   document.getElementById('emaill').innerText=email
+   document.getElementById('addresss').innerText=address
+   document.getElementById('cityy').innerText=city
+   document.getElementById('cardnumberr').innerText=cardnumber
+   document.getElementById('cardcvcc').innerText=cardcvc
+
+
+ }
+
---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+function getinfo(){
+ 
+    var Subject = document.getElementById("phone").value
+    var from = document.getElementById("email").value
+     
+    var mesage = document.getElementById("message").value
+   alert("We have received an email about " + Subject +" from " + from)
+  
+ }
+
+
+ 
+
+
